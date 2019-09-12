3ae6effb33ea040cc13e1490c1fbfeb619f1f094
Dave Honneffer <pearofducks@gmail.com>
2019-09-12 07:40:26 +0200

    Lint

--- dev/foo.js
+++ dev/foo.js
@@ -12,5 +12,5 @@ fetch('/posts', {
   body: JSON.stringify({ title: 'foo', body: 'bar', userId: 1 }),
   headers: { "Content-type": "application/json; charset=UTF-8" },
 })
-.then(response => response.json())
-.then(json => console.log(json))
+  .then(response => response.json())
+  .then(json => console.log(json))
