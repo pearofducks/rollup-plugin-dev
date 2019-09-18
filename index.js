449b8b948c9b54073b1366d9427503c2975aff87
Dave Honneffer <pearofducks@gmail.com>
2019-09-18 11:06:33 +0200

    Bind server

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import { app, initApp } from './server'
 export default (opts = {}) => ({
   name: 'dev-server',
   buildEnd() {
-    if (!app) initApp(opts)
+    if (!app) initApp.bind(this)(opts)
   }
 })
