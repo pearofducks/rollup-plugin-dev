b6110dd32bfb49c04720cfaec222ed0526993ce1
Dave Honneffer <pearofducks@gmail.com>
2021-07-18 08:59:17 +0200

    Add config test

---
+++ test/config.test.js
@@ -0,0 +1,50 @@
+import { suite } from 'uvu'
+import * as assert from 'uvu/assert'
+import { normalize, serverDefaults } from '#config'
+
+const Config = suite('Config')
+
+Config('has sane defaults', () => {
+  const config = normalize()
+  assert.is(config.dirs[0], '.')
+  assert.is(config.port, 8080)
+  assert.is(config.server, serverDefaults)
+})
+
+Config('normalizes a directory string', () => {
+  const config = normalize('dist')
+  assert.is(typeof config, 'object')
+  assert.is(config.dirs[0], 'dist')
+})
+
+Config('processes a config object', () => {
+  const config = normalize({
+    dirs: ['foo', 'bar'],
+    port: 9090
+  })
+  assert.is(config.dirs.length, 2)
+  assert.is(config.port, 9090)
+})
+
+Config('config object can have server options', () => {
+  const config = normalize({
+    port: 9090,
+    server: {
+      https: true
+    }
+  })
+  assert.is(config.server.https, true)
+  Object.entries(serverDefaults).forEach(([k, v]) => {
+    assert.is(config.server[k], v)
+  })
+  assert.is(config.port, 9090)
+})
+
+Config('throws on invalid attributes', () => {
+  const tryNormalize = () => normalize({
+    dirs: 'whoops'
+  })
+  assert.throws(tryNormalize)
+})
+
+Config.run()
--- test/index.js
+++ test/index.js
@@ -1,3 +1,4 @@
 import './dirs.test.js'
 import './spa.test.js'
 import './proxy.test.js'
+import './config.test.js'
