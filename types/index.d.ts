56da38ebf8d23da587efc635545999b86b9be4f3
Dave Honneffer <pearofducks@gmail.com>
2021-07-22 21:26:56 +0200

    Add types

---
+++ types/index.d.ts
@@ -0,0 +1,24 @@
+import { Plugin } from 'rollup'
+import { FastifyPluginCallback } from 'fastify'
+
+export interface ProxyItem {
+  from: string
+  to: string
+  opts?: object
+}
+
+export interface DevOptions {
+  silent?: boolean;
+  force?: boolean;
+  proxy: [ProxyItem];
+  dirs: [string];
+  dirname: string;
+  spa: (boolean | string);
+  port: number;
+  host: string;
+  basePath: string;
+  extend: FastifyPluginCallback;
+  server: object;
+}
+
+export default function dev(options?: (string | DevOptions)): Plugin
