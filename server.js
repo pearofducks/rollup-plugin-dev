0ab8edf6cecab10b8609b41c6b54a8824bf845b7
Dave Honneffer <pearofducks@gmail.com>
2021-07-24 11:32:16 +0200

    Work around bug in get-port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import getPort from 'get-port'
 
 export async function boot(config) {
   try {
-    const resolvedPort = await getPort({ port: [config.port, ...getPort.makeRange(8081, 9000)] })
+    const resolvedPort = await getPort({ host: (config.host ?? '127.0.0.0'), port: [config.port, ...getPort.makeRange(8081, 9000)] })
     const server = await init(config)
     await server.listen(resolvedPort, config.host)
   } catch (err) {
